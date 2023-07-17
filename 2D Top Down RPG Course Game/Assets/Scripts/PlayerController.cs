08930e575c3af0971437cd444ee2069b4988a398
Aris Wahyu <ariswn07@gmail.com>
2023-07-18 01:42:07 +0700

    Course 2 Selesai
    Player Movement dengan Framework Unity Input
    
    Set Pivot dan Adjustment Pivot agar realistic
    
    Adjustment arah player menghadap dengan input mouse dan posisi player

--- 2D Top Down RPG Course Game/Assets/Scripts/PlayerController.cs
+++ 2D Top Down RPG Course Game/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,23 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 1f;
     private Vector2 moveInput;
-    private PlayerControls playerControls;
+    private PlayerControls playerControls; // PlayerControls adalah class yang di generate sebelumnya jika namanya bukan player control maka berubah juga nama classnya
     private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake() 
     {
+        // Inisialisasi player control
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable() 
     {
+        // Harus Enable dan Disable namun player akan pergi ke semua scene jadi tidak perlu disable
         playerControls.Enable();
     }
 
@@ -27,16 +33,39 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate() 
     {
+        PlayerDirectionFacingAdjustment();
         Move();
     }
 
     private void PlayerInput()
     {
+        // untuk mendapatkan nilai dari input. Movement dan Move adalah nama dari maps framework terkadang bisa berbeda
         moveInput = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        animator.SetFloat("MoveX", moveInput.x);
+        animator.SetFloat("MoveY", moveInput.y);
+        
     }
 
     private void Move()
     {
         rb.MovePosition(rb.position + moveInput * (speed * Time.fixedDeltaTime));
+    }
+
+    private void PlayerDirectionFacingAdjustment()
+    {
+        // Untuk mendapatkan posisi mouse
+        Vector3 mousePos = Input.mousePosition;
+
+        // Untuk mengubah koordinat player dari world ke layar
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        // Untuk membandingkan jika posisi mouse lebih kecil atau berada di kiri maka sprite akan di flip
+        if(mousePos.x < playerScreenPoint.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else 
+            spriteRenderer.flipX = false;
     }
 }
